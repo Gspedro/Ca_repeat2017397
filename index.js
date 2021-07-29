ded22bd2179a5e7029279bf775c5c66433e2a728
Gspedro <gdesaopedro@icloud.com>
2021-07-29 20:09:52 +0000

    Creating a db connection

---
+++ index.js
@@ -0,0 +1,11 @@
+
+//Importing all the modules that was installed;
+
+const express = require ('express');
+const bodyParser = require(body-parser)
+const index = express = express();
+index.use(bodyParser.json());
+const path = require ('path');
+
+const db = require ("./db");
+const collection = "search";
